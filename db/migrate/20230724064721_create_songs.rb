e6771b3b0f712a92d035148e517edafaa571ff53
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-24 16:59:23 +0900

    #13 インデックス追加

--- db/migrate/20230724064721_create_songs.rb
+++ db/migrate/20230724064721_create_songs.rb
@@ -4,11 +4,13 @@ class CreateSongs < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.string :artist, null: false
       t.integer :duration_time, null: false
-      t.text :memo, null: false, default: ''
-      t.integer :transposition, null: false
+      t.text :memo, null: false
+      t.integer :transposition, null: false, default: 0, limit: 1
       t.string :cover_img, null: false, default: ''
 
       t.timestamps
     end
+    add_index :songs, :name
+    add_index :songs, [:name, :artist], unique: true
   end
 end
--- db/migrate/20230724064854_create_setlists.rb
+++ db/migrate/20230724064854_create_setlists.rb
@@ -6,5 +6,6 @@ class CreateSetlists < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    add_index :setlists, :title, unique: true
   end
 end
