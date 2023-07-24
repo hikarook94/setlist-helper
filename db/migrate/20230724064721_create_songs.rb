3ad9c73b6935beef4412852c9b8f552ea4186645
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-24 16:11:15 +0900

    Songテーブルに制約を追加

--- db/migrate/20230724064721_create_songs.rb
+++ db/migrate/20230724064721_create_songs.rb
@@ -1,12 +1,12 @@
 class CreateSongs < ActiveRecord::Migration[7.0]
   def change
     create_table :songs do |t|
-      t.string :name
-      t.string :artist
-      t.integer :duration_time
-      t.text :memo
-      t.integer :transposition
-      t.string :cover_img
+      t.string :name, null: false
+      t.string :artist, null: false
+      t.integer :duration_time, null: false
+      t.text :memo, null: false, default: ''
+      t.integer :transposition, null: false
+      t.string :cover_img, null: false, default: ''
 
       t.timestamps
     end
