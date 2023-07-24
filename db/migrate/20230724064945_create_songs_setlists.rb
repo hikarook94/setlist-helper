2361ea7b6d3545b44575bd81f82b4ccf0efd4892
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-24 16:13:44 +0900

    #13 中間テーブルに制約を追加

--- db/migrate/20230724064945_create_songs_setlists.rb
+++ db/migrate/20230724064945_create_songs_setlists.rb
@@ -3,7 +3,7 @@ class CreateSongsSetlists < ActiveRecord::Migration[7.0]
     create_table :songs_setlists do |t|
       t.references :song, null: false, foreign_key: true
       t.references :setlist, null: false, foreign_key: true
-      t.integer :position
+      t.integer :position, null: false
 
       t.timestamps
     end
