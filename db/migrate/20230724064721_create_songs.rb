4ba87bde2670e3dd266ee3c673cc899d4b64900a
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-25 10:03:37 +0900

    #13 制約を追加

--- db/migrate/20230724064721_create_songs.rb
+++ db/migrate/20230724064721_create_songs.rb
@@ -1,14 +1,16 @@
 class CreateSongs < ActiveRecord::Migration[7.0]
   def change
     create_table :songs do |t|
-      t.string :name
-      t.string :artist
-      t.integer :duration_time
-      t.text :memo
-      t.integer :transposition
-      t.string :cover_img
+      t.string :name, null: false
+      t.string :artist, null: false
+      t.integer :duration_time, null: false
+      t.text :memo, null: false
+      t.integer :transposition, null: false, default: 0, limit: 1
+      t.string :cover_img, null: false, default: ''
 
       t.timestamps
     end
+    add_index :songs, :name
+    add_index :songs, [:name, :artist], unique: true
   end
 end
--- db/migrate/20230724064854_create_setlists.rb
+++ db/migrate/20230724064854_create_setlists.rb
@@ -1,10 +1,11 @@
 class CreateSetlists < ActiveRecord::Migration[7.0]
   def change
     create_table :setlists do |t|
-      t.string :title
-      t.integer :target_duration_time
+      t.string :title, null: false
+      t.integer :target_duration_time, null: false, default: 0
 
       t.timestamps
     end
+    add_index :setlists, :title, unique: true
   end
 end
