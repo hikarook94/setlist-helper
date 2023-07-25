ec15427c7207164b0369c8609285529821646cc6
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-25 10:00:28 +0900

    #13 Song_Setlistテーブル作成

---
+++ db/migrate/20230725005740_create_song_setlists.rb
@@ -0,0 +1,11 @@
+class CreateSongSetlists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :song_setlists do |t|
+      t.references :song, null: false, foreign_key: true
+      t.references :setlist, null: false, foreign_key: true
+      t.integer :position, null: false
+
+      t.timestamps
+    end
+  end
+end
