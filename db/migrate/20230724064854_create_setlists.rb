d3f419ff59514e5a2942ec2420eb7ae36e234be8
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-25 15:34:54 +0900

    #13 デフォルト値を削除

--- db/migrate/20230724064854_create_setlists.rb
+++ db/migrate/20230724064854_create_setlists.rb
@@ -2,7 +2,7 @@ class CreateSetlists < ActiveRecord::Migration[7.0]
   def change
     create_table :setlists do |t|
       t.string :title, null: false
-      t.integer :target_duration_time, null: false, default: 0
+      t.integer :target_duration_time, null: false
 
       t.timestamps
     end
