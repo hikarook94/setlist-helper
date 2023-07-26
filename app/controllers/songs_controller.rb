5795b0fb670779fdfdb94e1231ca2ea1c04b23ea
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-26 13:56:07 +0900

    #78 曲の一覧機能追加

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class SongsController < ApplicationController
   def index
+    @songs = Song.order(name: :desc)
   end
 end
--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Songs", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/songs/index"
+RSpec.describe 'Songs', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/songs/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
