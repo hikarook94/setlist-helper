3e6ecbf7ee4e94faa489ca38f5e2b4410d82c61d
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-26 14:43:06 +0900

    msを変換するヘルパーを追加

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module SongsHelper
+  def mitunes_seconds(ms)
+    s = ms / 1000
+    minutes = s / 60
+    seconds = s % 60
+    "#{minutes} 分 #{seconds} 秒"
+  end
 end
