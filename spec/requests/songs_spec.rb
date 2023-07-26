5ad29b5a95b424347f0b4a07888dfcccc1ec0945
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-26 13:18:15 +0900

    Songコントローラーを追加

---
+++ spec/requests/songs_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Songs", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/songs/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
