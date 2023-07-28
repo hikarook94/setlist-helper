7eda3b9c76f398ee0cc3cc9127a2e14c8877399c
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-28 13:46:24 +0900

    #37 特定アーティストの曲一覧を見ている時はアーティスト名と戻るボタンを表示する

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,12 +3,12 @@
 class SongsController < ApplicationController
   def index
     @sort_by = params[:sort_by]
+    @artist = params[:filter_by]
     if @sort_by == 'artists'
       @artists = Song.distinct.pluck(:artist).sort
     else
       @songs = params[:filter_by] ? Song.where(artist: params[:filter_by]) : Song.order(name: :asc)
     end
-
   end
 
   def show
