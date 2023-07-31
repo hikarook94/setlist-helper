c16ed907b5959d74431c0519f58a12bcd8717363
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-07-31 09:43:08 +0900

    #30 新規作成アクション追加

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,4 +14,18 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params)
+
+    if @song.save!
+      redirect_to @song
+    else
+      render :new, :unprocessable_entity
+    end
+  end
 end
