aa5aa08afeadbc4487ccf5be6ea171dbe701711b
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-02 10:07:08 +0900

    #79 millisecondsにto_iを追加

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,10 +2,10 @@
 
 module SongsHelper
   def to_minutes(milli_seconds)
-    (milli_seconds / 1000) / 60
+    (milli_seconds.to_i / 1000) / 60
   end
 
   def to_remaining_seconds(milli_seconds)
-    (milli_seconds / 1000) % 60
+    (milli_seconds.to_i / 1000) % 60
   end
 end
