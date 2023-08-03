bf678ddba3b381a9f32a03ae28f39b41e7e5faa3
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-03 13:19:11 +0900

    #48 セットリスト取得コンポーネントを実装

--- app/javascript/components/Setlists.js
+++ app/javascript/components/Setlists.js
@@ -1,3 +1,35 @@
-import React from "react";
-const Setlists = () => <h1>セットリスト</h1>;
+import React, { useState, useEffect } from 'react';
+import SetlistsViewer from './SetlistsViewer';
+
+const Setlists = () => {
+    const [setlists, setSetlists] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await window.fetch('/api/setlists');
+          if (!response.ok) throw Error(response.statusText);
+          const data = await response.json();
+          setSetlists(data);
+        } catch (error) {
+          setIsError(true);
+          console.error(error);
+        }
+
+        setIsLoading(false);
+      };
+
+      fetchData();
+    }, []);
+
+  return (
+    <>
+      <h1 className="text-4xl">セットリスト</h1>
+      <SetlistsViewer setlists={setlists} />
+    </>
+  );
+};
+
 export default Setlists;
