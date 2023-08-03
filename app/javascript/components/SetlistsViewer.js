71cdc32ea2fab82ca4a5603f3b57dcd9cd4754de
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-03 15:14:45 +0900

    #48 セットリスト一覧に曲数と時間の情報を追加

--- app/javascript/components/SetlistsViewer.js
+++ app/javascript/components/SetlistsViewer.js
@@ -2,11 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SetlistsViewer = ({ setlists }) => {
+  const convertToHours = (milliSeconds) => {
+    const minutes = Math.ceil(milliSeconds / 1000) / 60
+    const hours = Math.trunc(minutes / 60)
+    const remaining_minutes = Math.ceil(minutes % 60)
+    return `${hours} 時間 ${remaining_minutes} 分`
+  }
+
   const renderSetlists = (setlistArray) => {
 
     return setlistArray.map((setlist) => (
       <li key={setlist.id}>
-        {setlist.title}
+        <div>{setlist.title}</div>
+        {setlist.songs.length}{' 曲 '}
+        {convertToHours(setlist.songs.reduce((accum, current) => accum + current.duration_time, 0))}
+        {' / '}
+        {convertToHours(setlist.target_duration_time)}
       </li>
     ));
   };
@@ -22,6 +33,10 @@ SetlistsViewer.propTypes = {
   setlists: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     title: PropTypes.string,
+    songs: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    }))
   })).isRequired,
 };
 
