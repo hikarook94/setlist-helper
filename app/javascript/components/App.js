67d1ccabc639533193eb9bdc0ac4bf8b568f6cb0
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-03 13:15:01 +0900

    #48 React Router追加

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import Setlists from './Setlists';
 
-const App = () => <Setlists />;
+const App = () => (
+  <Routes>
+    <Route path="setlists/*" element={<Setlists />} />
+  </Routes>
+)
 
 export default App;
