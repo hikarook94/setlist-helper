520b59f88979a1b037f2c7227b9d0e1632f379fd
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-03 09:38:07 +0900

    #48 bin/rails g controller site index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'site/index'
   root to: 'songs#index'
   resources :songs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
