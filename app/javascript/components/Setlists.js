445aeaa24514153a416a2e33a1efb3e3c44d4e6a
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-04 10:52:01 +0900

    #48 Setlistsのデータ取得部分を削除

--- app/javascript/components/Setlists.js
+++ app/javascript/components/Setlists.js
@@ -1,37 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import SetlistsViewer from './SetlistsViewer';
 import SetlistViewer from './SetlistViewer';
 
 const Setlists = () => {
-    const [setlists, setSetlists] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await window.fetch('/api/setlists');
-          if (!response.ok) throw Error(response.statusText);
-          const data = await response.json();
-          setSetlists(data);
-        } catch (error) {
-          setIsError(true);
-          console.error(error);
-        }
-
-        setIsLoading(false);
-      };
-
-      fetchData();
-    }, []);
 
   return (
     <>
       <h1 className="text-4xl">セットリスト</h1>
       <Routes>
-        <Route path="" element={<SetlistsViewer setlists={setlists} />} />
-        <Route path="/:id" element={<SetlistViewer setlists={setlists} />} />
+        <Route path="" element={<SetlistsViewer />} />
+        <Route path="/:id" element={<SetlistViewer />} />
       </Routes>
     </>
   );
