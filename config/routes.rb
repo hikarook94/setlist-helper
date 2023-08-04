4a7b86f1eb3c754a916788382c05925cb3e7bc20
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-04 11:01:52 +0900

    #48 siteへのルーティングをsetlistsに修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: redirect('/setlists')
   resources :songs
 
-  get 'setlists', to: 'site#index'
-  get 'setlists/new', to: 'site#index'
-  get 'setlists/:id', to: 'site#index'
-  get 'setlists/:id/edit', to: 'site#index'
+  get 'setlists', to: 'setlists#index'
+  get 'setlists/new', to: 'setlists#index'
+  get 'setlists/:id', to: 'setlists#index'
+  get 'setlists/:id/edit', to: 'setlists#index'
 
   namespace :api do
     resources :setlists
