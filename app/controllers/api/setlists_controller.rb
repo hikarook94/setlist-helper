c8a894206b8cb215d313f6a9676f53069bfe4940
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-04 16:11:06 +0900

    #48 rubocop -A

--- app/controllers/api/setlists_controller.rb
+++ app/controllers/api/setlists_controller.rb
@@ -3,6 +3,6 @@
 class Api::SetlistsController < ApplicationController
   def index
     @setlists = Setlist.includes(:songs).all
-    render json: @setlists.as_json(methods: [:songs_count, :total_duration_time])
+    render json: @setlists.as_json(methods: %i[songs_count total_duration_time])
   end
 end
--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -8,11 +8,9 @@ class Setlist < ApplicationRecord
   validates :target_duration_time, presence: true
   validates :target_duration_time, numericality: { only_integer: true }
 
-  def songs_count
-    self.songs.count
-  end
+  delegate :count, to: :songs, prefix: true
 
   def total_duration_time
-    self.songs.inject(0) {|sum, song| sum + song.duration_time}
+    songs.inject(0) { |sum, song| sum + song.duration_time }
   end
 end
