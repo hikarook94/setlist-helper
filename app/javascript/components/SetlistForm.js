9f3ba00c35fb45e82a6d287dc7baca7c1301acd2
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-07 09:46:17 +0900

    #100 セットリスト基本情報入力画面作成

---
+++ app/javascript/components/SetlistForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const SetlistForm = () => {
+  return (
+    <>
+      <div className="mb-8">
+        <label htmlFor="setlistTitle" className="mb-1">セットリスト名</label>
+        <div>
+          <input
+            name="setlistTitle"
+            className="border-0 ring-1 ring-inset rounded-md ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div className="mb-8">
+        <p className="mb-1">何時間分のセットリストを作成しますか？</p>
+        <input
+          id="setlistHours"
+          name="setlistHours"
+          className="mr-2 w-12 border-0 ring-1 ring-inset rounded-md ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+        <label htmlFor="setlistHours" className="mr-4">時間</label>
+        <input
+          id="setlistMinutes"
+          name="setlistMinutes"
+          className="mr-2 w-12 border-0 ring-1 ring-inset rounded-md ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+        <label htmlFor="setlistMinutes">分</label>
+      </div>
+      <div className="text-center">
+        <Link to="/setlists/new"><button className="align-center mx-0 rounded-full bg-blue-500 w-48">次へ</button></Link>
+      </div>
+    </>
+  )
+}
+export default SetlistForm
