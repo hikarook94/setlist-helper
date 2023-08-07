75757615337a76ff396a122d541a69030b492fac
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-07 09:39:25 +0900

    #100 セットリストのルーティングをApp.jsに集約

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import Setlist from './Setlist';
 import Setlists from './Setlists';
+import SetlistCreate from './SetlistCreate';
+import SetlistForm from './SetlistForm';
 
 const App = () => (
   <Routes>
-    <Route path="setlists/*" element={<Setlists />} />
+    <Route path="/setlists" element={<Setlists />} />
+    <Route path="/setlists/:id" element={<Setlist />} />
+    <Route path="/setlists/new" element={<SetlistCreate />}>
+      <Route path="form" element={<SetlistForm />} />
+    </Route>
   </Routes>
 )
 
