155d35e8dc652d62efd0bdff5eaffc0172c7640b
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-08 11:44:52 +0900

    #100 locationを使ってデータ読み出し

--- app/javascript/components/SetlistSongSelect.js
+++ app/javascript/components/SetlistSongSelect.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom'
 
 const SetlistSongSelect = () => {
+  const location = useLocation();
+  const inputValues = location.state.data
+  console.log(location)
 
   return (
     <>
-      <p>曲選択画面</p>
+      <div className="p-4">
+        <h1 className="text-2xl text-center mb-8">セットリスト新規作成</h1>
+        <p>曲選択画面</p>
+        <p>
+          {inputValues.setlistTitle}
+        </p>
+        <p>
+          {inputValues.setlistHours} 時間
+        </p>
+        <p>
+          {inputValues.setlistMinutes} 分
+        </p>
+      </div>
     </>
   );
 };
