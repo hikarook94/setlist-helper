8fb034a791ba744c5332a530ff6a224878fd48c5
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-08 11:43:07 +0900

    #100 不要なSetlistCreateコンポーネントを削除

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Setlist from './Setlist';
 import Setlists from './Setlists';
-import SetlistCreate from './SetlistCreate';
+import SetlistForm from './SetlistForm';
+import SetlistSongSelect from './SetlistSongSelect';
 
 const App = () => (
   <Routes>
     <Route path="/setlists" element={<Setlists />} />
     <Route path="/setlists/:id" element={<Setlist />} />
-    <Route path="/setlists/new" element={<SetlistCreate />} />
+    <Route path="/setlists/new" element={<SetlistForm />} />
+    <Route path="/setlists/new/songs" element={<SetlistSongSelect />} />
   </Routes>
 )
 
