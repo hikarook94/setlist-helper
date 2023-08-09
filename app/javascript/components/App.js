679cb306c7c16b9fc630174ceacaa2225d046555
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-09 20:06:47 +0900

    #47 コンテクストにフックを設定してコンポーネントに渡す

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Setlist from './Setlist';
 import Setlists from './Setlists';
 import SetlistForm from './SetlistForm';
 import SetlistSongSelect from './SetlistSongSelect';
+import {InputValueContext} from './InputValueContext';
 
-const App = () => (
-  <Routes>
-    <Route path="/setlists" element={<Setlists />} />
-    <Route path="/setlists/:id" element={<Setlist />} />
-    <Route path="/setlists/new" element={<SetlistForm />} />
-    <Route path="/setlists/new/songs" element={<SetlistSongSelect />} />
-  </Routes>
-)
+
+const App = () => {
+  const [inputValues, setInputValues] = useState();
+
+  return (
+    <Routes>
+      <Route path="/setlists" element={<Setlists />} />
+      <Route path="/setlists/:id" element={<Setlist />} />
+      <Route path="/setlists/new" element={
+        <InputValueContext.Provider value={{inputValues, setInputValues}}>
+          <SetlistForm />
+        </InputValueContext.Provider>
+      } />
+      <Route path="/setlists/new/songs" element={
+        <InputValueContext.Provider value={{inputValues, setInputValues}}>
+          <SetlistSongSelect />
+        </InputValueContext.Provider>
+      } />
+    </Routes>
+  )
+}
 
 export default App;
