993c8b905651df709911f4b70b41a692c28621af
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-10 18:41:38 +0900

    #47 リクエストパラメーターをランダム選曲するメソッドに渡す

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,6 @@
 class Api::SongsController < ApplicationController
   def random
-    @songs = Song.random([])
+    @songs = Song.random(params[:song_ids], Setlist.to_milliseconds(params[:setlistHours], params[:setlistMinutes]))
     render json: @songs.as_json
   end
 end
