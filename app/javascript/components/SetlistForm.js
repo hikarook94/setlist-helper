b3b3dea77676ccfc929a24052ccc5faf8e05f141
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-14 17:01:59 +0900

    #47 不要なimportを削除

--- app/javascript/components/SetlistForm.js
+++ app/javascript/components/SetlistForm.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom'
 import { yupResolver } from "@hookform/resolvers/yup"
