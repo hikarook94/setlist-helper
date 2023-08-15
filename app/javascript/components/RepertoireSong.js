5ddc139861568bca716d31dbe569f3d3f3538331
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-15 19:35:22 +0900

    #102 レパートリーの曲を表示するコンポーネントを追加

---
+++ app/javascript/components/RepertoireSong.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { convertToMinutes } from '../helpers/helpers'
+import { useInputValue, useUpdateInputValue } from './InputValueContext';
+
+function RepertoireSong( props ) {
+  const [ inputValues, setInputValues ] = useInputValue();
+  const updateInputValue = useUpdateInputValue();
+  const [isSelected, setIsSelected] = useState(false);
+
+  return (
+    <>
+      <div className="ml-12 mr-8 mb-1 px-4 py-2 border">
+        <li>
+          <div onClick={() => props.onSongSelected(props.value)}>+</div>
+          <div>
+            <div className="text-lg">{props.value.name}</div>
+            <div className="text-gray-400 text-sm">{props.value.artist}</div>
+            <div className="text-gray-400 text-sm">{convertToMinutes(props.value.duration_time)}</div>
+          </div>
+        </li>
+      </div>
+    </>
+  )
+}
+
+export default RepertoireSong;
