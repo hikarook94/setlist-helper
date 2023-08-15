59c68bb5c3f4c7f20bb07b0b2dd12a15e2a7bdf0
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-15 15:22:01 +0900

    #102 レパートリー一覧を返すアクションを追加

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Api::SongsController < ApplicationController
+  def index
+    @songs = Song.select(:id, :name, :artist, :duration_time)
+    render json: @songs
+  end
+
   def random
     setlist_limit = Setlist.to_milliseconds(params[:setlistHours], params[:setlistMinutes])
     @songs = Song.random(params[:song_ids], params[:total_duration_time], setlist_limit)
