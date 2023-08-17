d8210fa65266abaa6b283dd426f77dff60427a61
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-17 11:44:39 +0900

    #102 選択/非選択機能を追加

--- app/javascript/components/RepertoireSong.js
+++ app/javascript/components/RepertoireSong.js
@@ -4,13 +4,22 @@ import { useInputValue } from './InputValueContext';
 
 function RepertoireSong( props ) {
   const [ inputValues, setInputValues ] = useInputValue();
-  const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(props.isSelected);
+
+  const handleClick = () => {
+    setIsSelected(!isSelected)
+    props.onSongSelected(props.value)
+  }
 
   return (
-    <>
-      <div className="ml-12 mr-8 mb-1 px-4 py-2 border">
-        <li>
-          <div onClick={() => props.onSongSelected(props.value)}>+</div>
+    <div className="flex">
+        <div
+          className="text-xl w-16 text-center flex justify-center items-center"
+          onClick={handleClick}>
+          {!isSelected && <span>+</span>}
+          {isSelected && <span>✔️</span>}
+        </div>
+        <li className="mr-8 mb-1 px-4 py-2 border w-full">
           <div>
             <div className="text-lg">{props.value.name}</div>
             <div className="text-gray-400 text-sm">{props.value.artist}</div>
@@ -18,7 +27,6 @@ function RepertoireSong( props ) {
           </div>
         </li>
       </div>
-    </>
   )
 }
 
