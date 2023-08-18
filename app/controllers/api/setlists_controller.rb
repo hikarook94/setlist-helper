1fce0d6d5e77d295ab6d81b95bca34b92406fe95
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-18 14:08:06 +0900

    #102 セットリストの中間テーブル保存処理にトランザクションを追加

--- app/controllers/api/setlists_controller.rb
+++ app/controllers/api/setlists_controller.rb
@@ -7,15 +7,28 @@ class Api::SetlistsController < ApplicationController
   end
 
   def create
-    @setlist = Setlist.new(title: params[:title], target_duration_time: params[:target_duration_time])
-    songs = Song.find(params[:song_ids])
-    if @setlist.save
-      songs.each_with_index do |song, i|
-        @setlist.song_setlists.create(song:, position: i)
-      end
-      render json: @setlist
-    else
-      render json: @setlist.errors, status: :unprocessable_entity
+    @setlist = Setlist.new(setlist_params)
+    @songs = Song.find(params[:song_ids])
+
+    ActiveRecord::Base.transaction do
+      @setlist.save!
+      create_song_setlists!
+    end
+    render json: @setlist
+
+  rescue ActiveRecord::RecordInvalid => e
+    render json: e.record.errors, status: :unprocessable_entity
+  end
+
+  private
+
+  def setlist_params
+    params.require(:setlist).permit(:title, :target_duration_time)
+  end
+
+  def create_song_setlists!
+    @songs.each_with_index do |song, i|
+      @setlist.song_setlists.create!(song:, position: i)
     end
   end
 end
