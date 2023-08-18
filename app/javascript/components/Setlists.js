7ad172fd8381f10fe10e81cddee55169805b4dae
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-18 11:28:35 +0900

    #86 セットリスト欄だけがスクロールするように修正

--- app/javascript/components/Setlists.js
+++ app/javascript/components/Setlists.js
@@ -20,26 +20,29 @@ const Setlists = () => {
     fetchSetlists();
   }, []);
 
-  return (
-    <>
-      <h1 className="text-2xl text-center mb-4 pt-4">セットリスト</h1>
+    return (
+      <div className="h-screen">
+        <h1 className="text-2xl text-center mb-4 pt-4">セットリスト</h1>
 
-      <div className="p-4">
-        <ul>
-          <div className="mb-1 px-4 py-2 border">
-            <li>
-              <Link to="/setlists/new">
-                <div>新規作成</div>
-              </Link>
-            </li>
+        <div className="p-4 relative h-4/5 overflow-y-auto">
+          <div className="h-full">
+            <ul>
+              <div className="mb-1 px-4 py-2 border">
+                <li>
+                  <Link to="/setlists/new">
+                    <div>新規作成</div>
+                  </Link>
+                </li>
+
+              </div>
+              {setlists.map((setlist) => (
+                <ListedSetlist key={setlist.id} value={setlist} />
+              ))}
+            </ul>
           </div>
-          {setlists.map((setlist) => (
-            <ListedSetlist key={setlist.id} value={setlist} />
-          ))}
-        </ul>
+        </div>
       </div>
-    </>
-  );
+    )
 };
 
 export default Setlists;
