ff75f332c2aab737044b73cb1112e309ab5ea83a
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-18 11:22:21 +0900

    #86 スタイル調整

--- app/javascript/components/Setlist.js
+++ app/javascript/components/Setlist.js
@@ -27,30 +27,35 @@ const Setlist = () => {
 
   return (
     <>
-      <p className="text-2xl text-center mb-4">
-        {setlist.title}
-      </p>
-      <div className="text-center mb-4">
-        <div>
-          {setlist.songs_count}曲
+      <div className="h-screen">
+        <div className="flex relative justify-center items-center pt-6 mb-2 mx-4">
+          <Link to="/setlists">
+            <div className="w-16 absolute vertical-center top-6 left-1">＜</div>
+          </Link>
+          <h1 className="text-2xl">{setlist.title}</h1>
         </div>
-        <span>
-          {convertToHours(setlist.total_duration_time)}
-        </span>
-        <span>/</span>
-        <span>
-          {convertToHours(setlist.target_duration_time)}
-        </span>
-      </div>
-      <div className="relative h-4/5 overflow-y-auto mx-4 mb-8">
-        <div className="h-full">
-          <ul>
-            {
-              setlist.songs.map((song) => (
-                <ListedSong key={song.id} value={song} />
-              ))
-            }
-          </ul>
+        <div className="text-center mb-4">
+          <div className="mb-2">
+            {setlist.songs_count}曲
+          </div>
+          <span>
+            {convertToHours(setlist.total_duration_time)}
+          </span>
+          <span>/</span>
+          <span>
+            {convertToHours(setlist.target_duration_time)}
+          </span>
+        </div>
+        <div className="relative h-4/5 overflow-y-auto mx-4 mb-8">
+          <div className="h-full">
+            <ul>
+              {
+                setlist.songs.map((song, index) => (
+                  <SetlistSong key={song.id} value={song} index={index} />
+                ))
+              }
+            </ul>
+          </div>
         </div>
       </div>
     </>
