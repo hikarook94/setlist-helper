a8e7ed62f0009afed0f56acbefeb8a0f9fb6ba44
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-22 15:44:35 +0900

    #49 editアクション、updateアクションを追加

--- app/controllers/api/setlists_controller.rb
+++ app/controllers/api/setlists_controller.rb
@@ -25,10 +25,37 @@ class Api::SetlistsController < ApplicationController
     render json: e.record.errors, status: :unprocessable_entity
   end
 
+  def edit
+    @setlist = Setlist.includes(:songs).find(params[:id])
+    render json: @setlist.as_json(include: :songs, methods: %i[songs_count total_duration_time])
+  end
+
+  def update
+    @setlist = Setlist.includes(:songs).find(params[:id])
+
+    ActiveRecord::Base.transaction do
+      @setlist.update!(setlist_params)
+      update_song_setlists!(params[:song_ids])
+    end
+    render json: @setlist
+
+  rescue ActiveRecord::RecordInvalid => e
+    render json: e.record.errors, status: :unprocessable_entity
+  end
+
   private
 
   def setlist_params
     params.require(:setlist).permit(:title, :target_duration_time)
+  end
+
+  def update_song_setlists!(song_ids)
+    @setlist.song_setlists.where.not(song_id: song_ids).destroy_all
+
+    song_ids.each_with_index do |song_id, i|
+      song_setlist = @setlist.song_setlists.find_or_initialize_by(song_id:)
+      song_setlist.update!(song_id:, position: i)
+    end
   end
 
   def create_song_setlists!
