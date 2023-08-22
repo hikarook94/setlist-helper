1339a8e5e68aaaf22d1dcb17b2eddc673cfa3973
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-22 15:43:17 +0900

    #49 セットリスト編集コンポーネントをルーティングに追加

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Setlist from "./Setlist";
 import Setlists from "./Setlists";
 import SetlistCreate from "./SetlistCreate";
+import SetlistEdit from "./SetlistEdit";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/setlists" element={<Setlists />} />
       <Route path="/setlists/:id" element={<Setlist />} />
+      <Route path="/setlists/:id/edit" element={<SetlistEdit />} />
       <Route path="/setlists/new/*" element={<SetlistCreate />} />
     </Routes>
   );
