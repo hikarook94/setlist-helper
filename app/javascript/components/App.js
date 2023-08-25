2a93ce5e8cf9bc4a218465a936ff3ffd5a1fa3d5
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-25 13:29:59 +0900

    #49 toastをApp.jsに移動

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,15 +4,23 @@ import Setlist from "./Setlist";
 import Setlists from "./Setlists";
 import SetlistCreate from "./SetlistCreate";
 import SetlistEdit from "./SetlistEdit";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { injectStyle } from "react-toastify/dist/inject-style";
+
+injectStyle();
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/setlists" element={<Setlists />} />
-      <Route path="/setlists/:id" element={<Setlist />} />
-      <Route path="/setlists/:id/edit/*" element={<SetlistEdit />} />
-      <Route path="/setlists/new/*" element={<SetlistCreate />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/setlists" element={<Setlists />} />
+        <Route path="/setlists/:id" element={<Setlist />} />
+        <Route path="/setlists/:id/edit/*" element={<SetlistEdit />} />
+        <Route path="/setlists/new/*" element={<SetlistCreate />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
--- app/javascript/components/SetlistEdit.js
+++ app/javascript/components/SetlistEdit.js
@@ -3,17 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import SetlistSongEdit from "./SetlistSongEdit";
 import Repertoire from "./Repertoire";
 import { InputValueProvider } from "./InputValueContext";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { injectStyle } from "react-toastify/dist/inject-style";
-
-injectStyle();
 
 const SetlistEdit = () => {
   return (
     <>
       <div className="h-screen">
-        <ToastContainer />
         <InputValueProvider>
           <Routes>
             <Route path="/" element={<SetlistSongEdit />} />
