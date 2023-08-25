87adfe7ace9f16308b5d4606e79e59025622a4fa
Hikaru Uyama <hikaru.uyama@luxiar.com>
2023-08-25 09:25:46 +0900

    #112 新規作成ボタンを固定する

--- app/javascript/components/Setlists.js
+++ app/javascript/components/Setlists.js
@@ -23,21 +23,20 @@ const Setlists = () => {
   return (
     <div className="h-screen">
       <h1 className="text-2xl text-center mb-4 pt-4">セットリスト</h1>
-
-      <div className="p-4 relative h-4/5 overflow-y-auto">
-        <div className="h-full">
-          <ul>
-            <div className="mb-1 px-4 py-2 border">
-              <li>
-                <Link to="/setlists/new">
-                  <div>新規作成</div>
-                </Link>
-              </li>
-            </div>
-            {setlists.map((setlist) => (
-              <ListedSetlist key={setlist.id} setlist={setlist} />
-            ))}
-          </ul>
+      <div class="p-4">
+        <div className="mb-1 px-4 py-2 border">
+          <Link to="/setlists/new">
+            <div>新規作成</div>
+          </Link>
+        </div>
+        <div className="relative max-h-[600px] overflow-y-auto">
+          <div className="h-full">
+            <ul>
+              {setlists.map((setlist) => (
+                <ListedSetlist key={setlist.id} setlist={setlist} />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
