f536d87e2f46ebfefe91d612dd8a4afcddbdf835
hikarook94 <walrus4l145@icloud.com>
2023-09-15 00:14:13 +0900

    #25 Userモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_010924) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_143659) do
   create_table "scores", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "song_id", null: false
     t.float "rating", null: false
@@ -49,6 +49,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_010924) do
     t.datetime "updated_at", null: false
     t.index ["name", "artist"], name: "index_songs_on_name_and_artist", unique: true
     t.index ["name"], name: "index_songs_on_name"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "scores", "songs"
