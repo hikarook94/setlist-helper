eb8ca2589312a8c269531dda22574cea4036d661
hikarook94 <walrus4l145@icloud.com>
2023-09-18 16:16:25 +0900

    Userにnameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_143659) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_070217) do
   create_table "scores", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "song_id", null: false
     t.float "rating", null: false
@@ -59,7 +59,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_14_143659) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
