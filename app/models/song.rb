4baa3517e9fa3a73a0e6b145202d32ef2bc99545
hikarook94 <walrus4l145@icloud.com>
2023-09-28 23:43:04 +0900

    rubocop -A

--- app/models/song.rb
+++ app/models/song.rb
@@ -48,9 +48,9 @@ class Song < ApplicationRecord
       artist: track.artists.first.name,
       minutes: track.duration_ms / 60_000, # duration_msはミリ秒単位なので、分に変換
       seconds: (track.duration_ms / 1000) % 60, # 残りの秒数を取得
-      memo: "", 
-      cover_img: track.album.images.first["url"], # アルバムのカバー画像URL
-      transposition: 0 
+      memo: '',
+      cover_img: track.album.images.first['url'], # アルバムのカバー画像URL
+      transposition: 0
     }
   end
 end
